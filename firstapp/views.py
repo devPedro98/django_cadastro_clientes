97145caccb57a554e0f2269532ba5aa4887b5ec5
devPedro98 <pedroribeirobarrios@gmail.com>
2022-05-25 19:11:35 -0300

    segundo commit

--- firstapp/views.py
+++ firstapp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def pagina_inicial(request):
-    return render(request, 'firstapp/html/index.html')
+    return render(request, 'firstapp/html/model-page.html')

be1ffed31791db4e87a36946405cc1c30d9ade87
devPedro98 <pedroribeirobarrios@gmail.com>
2022-05-25 20:58:42 -0300

    puxando os dados do back-end

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.pagina_inicial),
+    path('teste/', views.pagina_teste),
 ]
--- firstapp/views.py
+++ firstapp/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 
+from .models import Cadastro_Clientes
+
 
 def pagina_inicial(request):
-    return render(request, 'firstapp/html/model-page.html')
+    cadastro = Cadastro_Clientes.objects.all()
+    dados = {'cadastros': cadastro}
+    return render(request, 'firstapp/html/model-page.html', dados)
+
+
+def pagina_teste(request):
+    cadastro = Cadastro_Clientes.objects.all()
+    dados = {'cadastros': cadastro}
+    return render(request, 'firstapp/html/teste.html', dados)

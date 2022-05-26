13811cd6e0bc9270aed11895e17e2070e945cd23
devPedro98 <pedroribeirobarrios@gmail.com>
2022-05-26 13:58:07 -0300

    C do CRUD feito

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.pagina_inicial),
+    path('submit/', views.submit_post),
     path('teste/', views.pagina_teste),
 ]
--- firstapp/views.py
+++ firstapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from .models import Cadastro_Clientes
 
@@ -13,3 +13,14 @@ def pagina_teste(request):
     cadastro = Cadastro_Clientes.objects.all()
     dados = {'cadastros': cadastro}
     return render(request, 'firstapp/html/teste.html', dados)
+
+
+def submit_post(request):
+    if request.POST:
+        nome = request.POST.get('nome_cliente')
+        email = request.POST.get('email_cliente')
+        celular = request.POST.get('celular_cliente')
+        cidade = request.POST.get('cidade_cliente')
+        Cadastro_Clientes.objects.create(
+            nome=nome, email=email, numero_celular=celular, cidade=cidade)
+    return redirect('/')
